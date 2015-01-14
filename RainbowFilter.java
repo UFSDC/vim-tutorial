aec412362e0f9e428c088fad074ff973483c017a
Nick Tardif <nicktardif@gmail.com>
2015-01-14 14:26:38 -0500

    Added sample files

---
+++ RainbowFilter.java
@@ -0,0 +1,78 @@
+package Filters;
+
+import android.graphics.Bitmap;
+
+public class RainbowFilter extends Filter {
+    private int value;
+    private int spacing;
+    private int range;
+
+    public RainbowFilter() {
+        this.name = "Rainbow";
+    }
+
+    public Bitmap filter(Bitmap bitmap) {
+        int stride = bitmap.getWidth();
+        int height = bitmap.getHeight();
+
+        value = 50;
+        spacing = stride / 3;
+        range = spacing / 3;
+
+        int[] pixels = new int[bitmap.getWidth() * height];
+        bitmap.getPixels(pixels, 0, stride, 0, 0, stride, height);
+
+        int rBase = 1 * range;
+        int gBase = 2 * range;
+        int bBase = 3 * range;
+
+        for(int x = 0; x < bitmap.getWidth(); x++) {
+
+            int r = rainbowCalc(x, rBase);
+            int g = rainbowCalc(x, gBase);
+            int b = rainbowCalc(x, bBase);
+
+            for(int y = 0; y < bitmap.getHeight(); y++) {
+                int pixel = pixels[y * stride + x];
+
+                pixels[y * stride + x] =
+                    0xFF000000 |
+                    (((r + (( pixel >> 16)    & 0xFF)) / 2) << 16) |
+                    (((g + (( pixel >>  8)    & 0xFF)) / 2) << 8) |
+                    (((b + (( pixel >>  0)    & 0xFF)) / 2) << 0);
+            }
+        }
+        bitmap.setPixels(pixels, 0, stride, 0, 0, stride, height);
+        return bitmap;
+    }
+
+    private int rainbowCalc(int x, int base) {
+        int interval = x / spacing;
+
+        int max = interval * spacing + base;
+        int max1 = (interval - 1) * spacing + base;
+        int max2 = (interval + 1) * spacing + base;
+        int offset = Math.min(Math.min(Math.abs(x - max), Math.abs(x - max1)), Math.abs(x - max2));
+
+        if (offset >= 0 && offset <= range) {
+            return value + (int) (120 * ((float) (range - offset) / (float)range));
+        }
+
+        return value;
+    }
+
+	private int[] fillArray(int size) {
+		int[] myArray = new int[size];
+
+		// Easy way
+		for(int i = 0; i < size; i++) {
+			myArray[i] = i;
+		}
+
+		myArray[0] = 0;
+		myArray[1] = 1;
+		myArray[2] = 2;
+		myArray[3] = 3;
+		myArray[4] = 4;
+	}
+}
---
+++ syntastic.py
@@ -0,0 +1,15 @@
+"""Run the main function"""
+if __name__ == '__main__':
+    main()
+
+def main():
+    """The main function"""
+    print nicks_add(10, 3)
+
+def nicks_add(number_one, number_two):
+    """Adds a and b together
+    number_one -- a number
+    number_two -- another number
+    return -- number_one + number_two
+    """
+    return number_one + number_two
